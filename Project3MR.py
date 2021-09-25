c9795a63edd980643ce5dccaf150bf82656e142a
Marco Ramirez <89946288+mram6525@users.noreply.github.com>
2021-09-25 11:50:48 -0500

    Update Project3MR.py
    Code for downloading log file

--- Project3MR.py
+++ Project3MR.py
@@ -1,4 +1,9 @@
-#Download Data
+from urllib.request import urlretrieve
+
+url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+filename = 'localcopy.log'
+
+filename, headers = urlretrieve(url, filename)
 
 #Read Data
 
